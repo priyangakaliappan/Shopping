2d2c3f57df0c700c051c4d6228e8f638fdccb766
subhapriya <subhapragis@gmail.com>
2016-05-26 15:06:57 +0530

    changes

---
+++ src/main/java/com/github/elizabetht/model/Product.java
@@ -0,0 +1,43 @@
+package com.github.elizabetht.model;
+
+import java.util.Date;
+
+public class Product {
+
+private int productId;
+private String productName;
+private short isActive;
+private Date rowCreated;
+
+public int getProductId() {
+	return productId;
+}
+public void setProductId(int productId) {
+	this.productId = productId;
+}
+public String getProductName() {
+	return productName;
+}
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+public short getIsActive() {
+	return isActive;
+}
+public void setIsActive(short isActive) {
+	this.isActive = isActive;
+}
+public Date getRowCreated() {
+	return rowCreated;
+}
+public void setRowCreated(Date rowCreated) {
+	this.rowCreated = rowCreated;
+}
+public Date getRowAltered() {
+	return rowAltered;
+}
+public void setRowAltered(Date rowAltered) {
+	this.rowAltered = rowAltered;
+}
+private Date rowAltered;
+}
