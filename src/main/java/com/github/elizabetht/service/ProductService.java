2a51899e23651acce6d7673b6e8dfbc82e254495
priyangakaliappan <priyangajava@gmail.com>
2016-05-27 16:18:51 +0530

    changes added

--- src/main/java/com/github/elizabetht/service/ProductService.java
+++ src/main/java/com/github/elizabetht/service/ProductService.java
@@ -1,12 +1,15 @@
 package com.github.elizabetht.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.github.elizabetht.model.Product;
 
 public interface ProductService {
-	
 	void insertProduct(Product product);
+
 	ArrayList<Product> getAllList();
+	List<Product> allProducts();
+	Product getProductById(int productId);
 	
 }
